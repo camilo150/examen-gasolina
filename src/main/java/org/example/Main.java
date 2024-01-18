268f1a77ff2fe7f7bde438c942c186c19e4623e6
camilo castrillon <141888939+camilo150@users.noreply.github.com>
2024-01-18 16:11:22 -0500

    examen gasolina

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,27 @@
 package org.example;
-
-
 import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
-           /*mostrar el consumo total de combustible para
-        el viaje y el costo total de este combustible*/ /*litros por casda 100km*/
-        double km, precioGasolina,precioLitro=5000,gasolinaConsumida;
+
+        double gatosGasolinakm, km,precioLitro=5000,multplicacionGasolina;
         Scanner leerDato=new Scanner(System.in);
-        System.out.println("ingrese el numero de km recorridos");
-        km=leerDato.nextDouble();
         System.out.println("el precio de la gasolina por litro es de 5000");
-        System.out.println("ingrese la cantidad de gasolina consumida en lintros");
-        gasolinaConsumida= leerDato.nextDouble();
-        double precioEstimado =precioLitro*gasolinaConsumida;
-        double totalPrecio= precioEstimado*km;
-        System.out.println("la distancia recorrida es de"+km+"km, su consumo de gasolina es de "+gasolinaConsumida+" el total del precio de la gasolina consumida es de" +precioEstimado+ "litros, y el total de su costo por todo es:" +totalPrecio);
+
+        System.out.println("ingrese el numero de gasolina gastada por cada 100km");
+        gatosGasolinakm=leerDato.nextDouble();
+
+        multplicacionGasolina= precioLitro*precioLitro;
+
+        System.out.println("ingrese la cantidad de distancia que vas a conducir");
+        km= leerDato.nextDouble();
+        double gasolinaEstimada =km*gatosGasolinakm/100;
+        double precioEstimado= gasolinaEstimada*precioLitro;
+   
+        System.out.println("la distancia recorrida es de "+km+"km, el total del precio de la gasolina consumida es de" +gasolinaEstimada+ "litros, y el total de su costo por todo es:" +precioEstimado);
     }
+
+
+
 }
+
